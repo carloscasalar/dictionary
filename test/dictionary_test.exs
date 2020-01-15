efcc65fdd42fea935f8d8ec94ab828f7f6414f59
Carlos Castillo <castillo.st@gmail.com>
2020-01-15 08:36:03 +0100

    Change the dictionary interface Now it has two functions: start and random_word

--- test/dictionary_test.exs
+++ test/dictionary_test.exs
@@ -3,6 +3,7 @@ defmodule DictionaryTest do
   doctest Dictionary
 
   test "random_word returns a word" do
-    assert is_bitstring(Dictionary.random_word)
+    words = Dictionary.start()
+    assert is_bitstring(Dictionary.random_word(words))
   end
 end
